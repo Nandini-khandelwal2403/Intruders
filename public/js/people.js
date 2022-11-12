b64b3a42c355708eefefb3cd8d2fe71988fc3e13
Nandini Khandelwal <nandinikhandewal20.set@modyuniversity.ac.in>
2022-11-13 03:41:35 +0530

    highchart changes

--- public/js/people.js
+++ public/js/people.js
@@ -28,28 +28,28 @@ Highcharts.chart('container', {
         name: 'Highsoft',
         keys: ['from', 'to'],
         data: [
-            ['Shareholders', 'Board'],
-            ['Board', 'CEO'],
-            ['CEO', 'CTO'],
-            ['CEO', 'CPO'],
-            ['CEO', 'CSO'],
-            ['CEO', 'CMO'],
-            ['CEO', 'HR'],
-            ['CTO', 'Product'],
-            ['CTO', 'Web'],
-            ['CSO', 'Sales'],
-            ['CMO', 'Market']
+            ['COSA', 'COSA President'],
+            ['COSA President', 'Events Head'],
+            ['COSA President', 'Treasurer'],
+            // ['CEO', 'CPO'],
+            // ['CEO', 'CSO'],
+            // ['CEO', 'CMO'],
+            // ['CEO', 'HR'],
+            // ['CTO', 'Product'],
+            // ['CTO', 'Web'],
+            // ['CSO', 'Sales'],
+            // ['CMO', 'Market']
         ],
         levels: [{
             level: 0,
-            color: 'silver',
+            color: '#980104',
             dataLabels: {
                 color: 'black'
             },
             height: 25
         }, {
             level: 1,
-            color: 'silver',
+            color: '#3822C9',
             dataLabels: {
                 color: 'black'
             },
@@ -62,63 +62,68 @@ Highcharts.chart('container', {
             color: '#359154'
         }],
         nodes: [{
-            id: 'Shareholders'
-        }, {
-            id: 'Board'
-        }, {
-            id: 'CEO',
-            title: 'CEO',
-            name: 'Grethe Hjetland',
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132317/Grethe.jpg'
-        }, {
-            id: 'HR',
-            title: 'HR/CFO',
-            name: 'Anne Jorunn Fjærestad',
-            color: '#007ad0',
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132314/AnneJorunn.jpg',
-            column: 3,
-            offset: '75%'
-        }, {
-            id: 'CTO',
-            title: 'CTO',
-            name: 'Christer Vasseng',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12140620/Christer.jpg',
-            layout: 'hanging'
-        }, {
-            id: 'CPO',
-            title: 'CPO',
-            name: 'Torstein Hønsi',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12131849/Torstein1.jpg'
-        }, {
-            id: 'CSO',
-            title: 'CSO',
-            name: 'Anita Nesse',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132313/Anita.jpg',
-            layout: 'hanging'
-        }, {
-            id: 'CMO',
-            title: 'CMO',
-            name: 'Vidar Brekke',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/13105551/Vidar.jpg',
-            layout: 'hanging'
-        }, {
-            id: 'Product',
-            name: 'Product developers'
-        }, {
-            id: 'Web',
-            name: 'General tech',
-            description: 'Web developers, sys admin'
-        }, {
-            id: 'Sales',
-            name: 'Sales team'
-        }, {
-            id: 'Market',
-            name: 'Marketing team'
-        }],
+                id: 'COSA'
+            },
+            // }, {
+            //     id: 'Board'
+            // },
+            // {
+            //     id: 'CEO',
+            //     title: 'CEO',
+            //     name: 'Grethe Hjetland',
+            //     image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132317/Grethe.jpg'
+            // },
+            {
+                id: 'COSA President',
+                title: 'HR/CFO',
+                name: 'Anne Jorunn Fjærestad',
+                color: '#007ad0',
+                image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132314/AnneJorunn.jpg',
+                column: 3,
+                offset: '75%'
+            }, {
+                id: 'Events Head',
+                title: 'CTO',
+                name: 'Christer Vasseng',
+                column: 4,
+                image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12140620/Christer.jpg',
+                layout: 'hanging'
+            }, {
+                id: 'Treasurer',
+                title: 'CPO',
+                name: 'Torstein Hønsi',
+                column: 4,
+                image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12131849/Torstein1.jpg'
+            }
+            // , {
+            //     id: 'CSO',
+            //     title: 'CSO',
+            //     name: 'Anita Nesse',
+            //     column: 4,
+            //     image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132313/Anita.jpg',
+            //     layout: 'hanging'
+            // }, {
+            //     id: 'CMO',
+            //     title: 'CMO',
+            //     name: 'Vidar Brekke',
+            //     column: 4,
+            //     image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/13105551/Vidar.jpg',
+            //     layout: 'hanging'
+            // }, {
+            //     id: 'Product',
+            //     name: 'Product developers'
+            // }, {
+            //     id: 'Web',
+            //     name: 'General tech',
+            //     description: 'Web developers, sys admin'
+            // }, {
+            //     id: 'Sales',
+            //     name: 'Sales team'
+            // }, {
+            //     id: 'Market',
+            //     name: 'Marketing team'
+            // }
+        ],
         colorByPoint: false,
         color: '#007ad0',
         dataLabels: {
