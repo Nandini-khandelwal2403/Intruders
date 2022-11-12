85b3908a60aae9e9154b93b90146dc7a5a95772a
Debajyoti Halder <debajyotih@iitbhilai.ac.in>
2022-11-12 15:26:37 -0500

    people.js

--- public/js/people.js
+++ public/js/people.js
@@ -15,7 +15,7 @@ Highcharts.chart('container', {
     chart: {
         backgroundColor: 'transparent',
         height: 600,
-        width: 1200,
+        // width: 1200,
         inverted: true
     },
 
@@ -28,96 +28,56 @@ Highcharts.chart('container', {
         name: 'Highsoft',
         keys: ['from', 'to'],
         data: [
-            ['Shareholders', 'Board'],
-            ['Board', 'CEO'],
-            ['CEO', 'CTO'],
-            ['CEO', 'CPO'],
-            ['CEO', 'CSO'],
-            ['CEO', 'CMO'],
-            ['CEO', 'HR'],
-            ['CTO', 'Product'],
-            ['CTO', 'Web'],
-            ['CSO', 'Sales'],
-            ['CMO', 'Market']
+            ['COSA', 'COSA President'],
+            ['COSA President', 'Events Head'],
+            ['COSA President', 'Treasurer'],
+            // ['COSA President', 'S'],
         ],
         levels: [{
-            level: 0,
-            color: 'silver',
-            dataLabels: {
-                color: 'black'
+                level: 0,
+                color: 'silver',
+                dataLabels: {
+                    color: 'black'
+                },
+                height: 25
+            }, {
+                level: 1,
+                color: 'silver',
+                dataLabels: {
+                    color: 'black'
+                },
+                height: 25
+            }, {
+                level: 2,
+                color: '#980104'
             },
-            height: 25
-        }, {
-            level: 1,
-            color: 'silver',
-            dataLabels: {
-                color: 'black'
-            },
-            height: 25
-        }, {
-            level: 2,
-            color: '#980104'
-        }, {
-            level: 4,
-            color: '#359154'
-        }],
+            // {
+            //     level: 4,
+            //     color: '#359154'
+            // }
+        ],
         nodes: [{
-            id: 'Shareholders'
+            id: 'COSA'
         }, {
-            id: 'Board'
-        }, {
-            id: 'CEO',
-            title: 'CEO',
-            name: 'Grethe Hjetland',
+            id: 'COSA President',
+            title: 'COSA President',
+            name: 'Grethe',
             image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132317/Grethe.jpg'
         }, {
-            id: 'HR',
-            title: 'HR/CFO',
+            id: 'Events Head',
+            title: 'Events Head',
             name: 'Anne Jorunn Fjærestad',
             color: '#007ad0',
             image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132314/AnneJorunn.jpg',
-            column: 3,
-            offset: '75%'
+            // column: 3,
+            // offset: '75%'
         }, {
-            id: 'CTO',
+            id: 'Treasurer',
             title: 'CTO',
             name: 'Christer Vasseng',
-            column: 4,
+            // column: 4,
             image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12140620/Christer.jpg',
-            layout: 'hanging'
-        }, {
-            id: 'CPO',
-            title: 'CPO',
-            name: 'Torstein Hønsi',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12131849/Torstein1.jpg'
-        }, {
-            id: 'CSO',
-            title: 'CSO',
-            name: 'Anita Nesse',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/12132313/Anita.jpg',
-            layout: 'hanging'
-        }, {
-            id: 'CMO',
-            title: 'CMO',
-            name: 'Vidar Brekke',
-            column: 4,
-            image: 'https://wp-assets.highcharts.com/www-highcharts-com/blog/wp-content/uploads/2018/11/13105551/Vidar.jpg',
-            layout: 'hanging'
-        }, {
-            id: 'Product',
-            name: 'Product developers'
-        }, {
-            id: 'Web',
-            name: 'General tech',
-            description: 'Web developers, sys admin'
-        }, {
-            id: 'Sales',
-            name: 'Sales team'
-        }, {
-            id: 'Market',
-            name: 'Marketing team'
+            // layout: 'hanging'
         }],
         colorByPoint: false,
         color: '#007ad0',
